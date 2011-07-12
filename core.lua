f41ee66773efb68017c21b50fbfff9c21df8ecd4
Rick Warrington <phial1020@gmail.com>
2011-07-12 13:29:20 -0600

    Fixed button display problems and relocated it.

--- core.lua
+++ core.lua
@@ -1,5 +1,5 @@
-local button = CreateFrame("Button" "gMailButton", InboxFrame);
-button:SetPoint("BOTTOM", InboxFrame, "BOTTOM", 0, 10);
+local button = CreateFrame("Button", "gMailButton", InboxFrame, "UIPanelButtonTemplate");
+button:SetPoint("BOTTOM", InboxFrame, "BOTTOM", -10, 85);
 button:SetWidth(128);
 button:SetHeight(24);
 
