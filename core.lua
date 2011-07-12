59c4303a2f4228b18d09c845eefd9bb67d31243c
Rick Warrington <phial1020@gmail.com>
2011-07-12 13:29:50 -0600

    Working codes.

--- core.lua
+++ core.lua
@@ -7,16 +7,57 @@ local buttonText = button:CreateFontString(nil, "Overlay");
 buttonText:SetFontObject(GameFontNormalSmall);
 buttonText:SetPoint("CENTER");
 
-local function OnEvent(self, event, ...)
+local looting = false;
 
+local function UpdateButtonText()
+	local mailCount = GetInboxNumItems();
+	local moneyCount = 0;
+	local itemsCount = 0;
+	
+	for i = 1, mailCount do
+		local packageIcon, stationeryIcon, sender, subject, money, CODAmount, daysLeft, hasItem, wasRead, wasReturned, textCreated, canReply, isGM = GetInboxHeaderInfo(i);
+		moneyCount = moneyCount + money;
+		if(hasItem and CODAmount < 1) then
+			itemsCount = itemsCount + hasItem;
+		end
+	end
+	
+	buttonText:SetText(format("%d gold, %d items", floor(moneyCount * 0.0001), itemsCount))
+end
+
+local function OnEvent(self, event, ...)
+	if(event == "MAIL_INBOX_UPDATE") then
+		UpdateButtonText();
+	end
+	
+	if(looting == true) then
+		local mailCount = GetInboxNumItems();
+		if(mailCount == 0) then
+			looting = false;
+			return;
+		end
+		
+		for i = mailCount, 1, -1 do
+			local packageIcon, stationeryIcon, sender, subject, money, CODAmount, daysLeft, hasItem, wasRead, wasReturned, textCreated, canReply, isGM = GetInboxHeaderInfo(i);
+			if(hasItem and CODAmount < 1) then
+				TakeInboxItem(i);
+				return;
+			end
+			if(money > 0) then
+				TakeInboxMoney(i);
+				return;
+			end
+		end
+	end
 end
 
 local function OnClick()
-
+	looting = true;
+	OnEvent();
 end
 
 local function OnHide()
-
+	looting = false;
 end
 
 button:RegisterEvent("MAIL_INBOX_UPDATE");
