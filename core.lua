d471fb51a562550d3f9ec17ea104575a56367d8b
Rick Warrington <phial1020@gmail.com>
2011-07-12 12:35:23 -0600

    Initial Commit

---
+++ core.lua
@@ -0,0 +1,25 @@
+local button = CreateFrame("Button" "gMailButton", InboxFrame);
+button:SetPoint("BOTTOM", InboxFrame, "BOTTOM", 0, 10);
+button:SetWidth(128);
+button:SetHeight(24);
+
+local buttonText = button:CreateFontString(nil, "Overlay");
+buttonText:SetFontObject(GameFontNormalSmall);
+buttonText:SetPoint("CENTER");
+
+local function OnEvent(self, event, ...)
+
+end
+
+local function OnClick()
+
+end
+
+local function OnHide()
+
+end
+
+button:RegisterEvent("MAIL_INBOX_UPDATE");
+button:SetScript("OnEvent", OnEvent);
+button:SetScript("OnClick", OnClick);
+button:SetScript("OnHide", OnHide);
